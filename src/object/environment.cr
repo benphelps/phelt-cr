c26cda35f1df30a9cd298f800d9d31079dbd0f5a
Ben Phelps <ben@phelps.io>
2020-08-16 14:00:36 +0300

    fix deeply nested outer environments
    this is a whole thing now

--- src/object/environment.cr
+++ src/object/environment.cr
@@ -12,18 +12,23 @@ module PheltObject
 
     def get(name : ::String)
       return @store[name] if @store.has_key? name
-      outer = @outer
-      unless outer.nil?
+      return get_from_outer(name, @outer)
+    end
+
+    def get_from_outer(name : ::String, outer : PheltObject::Environment?)
+      if outer.is_a? PheltObject::Environment
         return outer.store[name] if outer.store.has_key? name
+        if outer.outer.is_a? PheltObject::Environment
+          return get_from_outer(name, outer.outer)
+        end
       end
       return PheltObject::Error.new("error", "error", 0, 0)
     end
 
     def exists?(name : ::String) : Bool
       return true if @store.has_key? name
-      outer = @outer
-      unless outer.nil?
-        return true if outer.store.has_key? name
+      if find_outer(name, @outer)
+        return true
       end
       return false
     end
@@ -37,19 +42,24 @@ module PheltObject
       return false
     end
 
+    def find_outer(name : ::String, outer : PheltObject::Environment?) : PheltObject::Environment?
+      if outer.is_a? PheltObject::Environment
+        return outer if outer.store.has_key? name
+        if outer.outer.is_a? PheltObject::Environment
+          return find_outer(name, outer.outer)
+        end
+      end
+      return nil
+    end
+
     def set(name : ::String, value : PheltObject::Object, const = false)
       @constants << name if const
 
       if @scoped
         @store[name] = value
       else
-        outer = @outer
-        if !outer.nil?
-          if outer.store.has_key? name
-            outer.store[name] = value
-          else
-            @store[name] = value
-          end
+        if outer = find_outer(name, @outer)
+          outer.store[name] = value
         else
           @store[name] = value
         end
